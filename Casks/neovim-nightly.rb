35fa4d485bbfa4b2a734f2b93f31c58bebd7579c
Heiko Zimmermann <23041469+zmheiko@users.noreply.github.com>
2024-10-08 17:29:16 +0200

    Update neovim-nightly.rb

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -1,10 +1,13 @@
 cask 'neovim-nightly' do
+  arch arm:   "arm64",
+       intel: "x86_64"
+
   version :latest
   sha256 :no_check
 
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz"
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz"
   name 'Neovim Nightly'
   homepage 'https://neovim.io/'
 
-  binary 'nvim-macos/bin/nvim'
+  binary "nvim-macos-#{arch}/bin/nvim"
 end
